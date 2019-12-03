05ff695fb31111d2308c845f020c75f84a09f524
Emmi Nilsson <emmie.nilsson@gmail.com>
2019-12-03 13:30:09 +0100

    Handling form and posting to API

--- code/src/App.js
+++ code/src/App.js
@@ -2,13 +2,44 @@ import React, { useState, useEffect } from 'react'
 
 export const App = () => {
   const [thoughts, setThoughts] = useState([])
+  const [newThought, setNewThought] = useState("")
+
+
+  // Get data from API
   useEffect(() => {
     fetch('https://technigo-thoughts.herokuapp.com/')
       .then(response => response.json())
       .then(json => setThoughts(json))
   }, [])
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault()
+
+    // Send post request to api
+    fetch('https://technigo-thoughts.herokuapp.com/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ message: newThought })
+    })
+      .then((response) => response.json())
+      .then((newThought) => {
+        setThoughts((previousThoughts) => [newThought, ...previousThoughts])
+      })
+  }
+
   return (
     <div>
+      <div><p>{newThought}</p></div>
+      <form onSubmit={handleFormSubmit}>
+        <label>
+          <span>What is making you happy right now?</span>
+          <input
+            type="text"
+            onChange={(event) => setNewThought(event.target.value)}
+            value={newThought} />
+        </label>
+        <button type="submit">Send Happy Thought</button>
+      </form>
       {thoughts.map(thought => (
         <div key={thought._id}>
           <p>{thought.message} {thought.id}</p>
