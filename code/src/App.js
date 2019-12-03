626dc43b66d8183b9fd97faab4a32af25366465a
Emmi Nilsson <emmie.nilsson@gmail.com>
2019-12-03 10:43:39 +0100

    Displaying data from API on page

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,21 @@
 import React, { useState, useEffect } from 'react'
 
 export const App = () => {
+  const [thoughts, setThoughts] = useState([])
   useEffect(() => {
     fetch('https://technigo-thoughts.herokuapp.com/')
       .then(response => response.json())
-      .then(json => console.log(json))
-  })
+      .then(json => setThoughts(json))
+  }, [])
   return (
     <div>
-      This will be a Happy thoughts app.
+      {thoughts.map(thought => (
+        <div key={thought._id}>
+          <p>{thought.message} {thought.id}</p>
+          <p>{thought.hearts}</p>
+          <p>{thought.createdAt}</p>
+        </div>
+      ))}
     </div>
   )
 }
